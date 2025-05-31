f3f5af66e362729440df76e2b1c76a2f85476a3f
mmeguizo <mark.meguizo@gmail.com>
2025-05-31 18:57:32 +0800

    done builk

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const NAVIGATION: Navigation = [
   },
   {
     segment: 'imports',
-    title: 'Import/Export',
+    title: 'Import',
     icon: <ImportExportIcon />,
     // pattern: 'employees{/:employeeId}*',
   },
